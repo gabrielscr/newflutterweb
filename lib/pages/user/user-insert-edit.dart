22137d20378758b59ce2b1f156acb2d53002e541
Gabriel Rocha <gabrielscrocha4@gmail.com>
2019-04-22 16:10:06 -0300

    streambuilder

--- lib/pages/user/user-insert-edit.dart
+++ lib/pages/user/user-insert-edit.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:flutter/material.dart';
 import 'package:newflutterproject/domain/user.dart';
 import 'package:newflutterproject/pages/image/image-handler.dart';
@@ -16,7 +15,6 @@ class UserInsertEdit extends StatefulWidget {
 class _UserInsertEdit extends State<UserInsertEdit>
     with TickerProviderStateMixin, ImagePickerListener {
   final int userId;
-  bool _saving = false;
 
   var user = User();
 
@@ -65,9 +63,11 @@ class _UserInsertEdit extends State<UserInsertEdit>
       txtPassword.text = userFromServer.password;
     }
 
-    setState(() {
-      user = userFromServer;
-    });
+    if (mounted) {
+      setState(() {
+        user = userFromServer;
+      });
+    }
   }
 
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
@@ -82,119 +82,118 @@ class _UserInsertEdit extends State<UserInsertEdit>
       appBar: AppBar(
         title: Text(appBarText),
       ),
-      body: ModalProgressHUD(
-        progressIndicator: RefreshProgressIndicator(),
-        inAsyncCall: _saving,
-        child: renderForm()
-      ),
+      body: Container(
+          child: renderForm()),
     );
   }
 
-  Widget renderForm(){
+  Widget renderForm() {
     final Size screenSize = MediaQuery.of(context).size;
 
     var fotoPerfil = this.user.image;
 
     return Form(
-          key: _formKey,
-          autovalidate: true,
-          child: new ListView(
-            children: <Widget>[
-              new GestureDetector(
-                onTap: () => imagePicker.showDialog(context),
-                child: new Center(
-                  child: _image == null
-                      ? new Stack(
-                          children: <Widget>[
-                            new Center(
-                              child: new CircleAvatar(
-                                radius: 80.0,
-                                backgroundImage: fotoPerfil == null
-                                    ? AssetImage('assets/img/male.png')
-                                    : AssetImage(fotoPerfil),
-                                backgroundColor: const Color(0xFF778899),
-                              ),
-                            ),
-                          ],
-                        )
-                      : new Container(
-                          height: 160.0,
-                          width: 160.0,
-                          decoration: new BoxDecoration(
-                            color: const Color(0xff7c94b6),
-                            image: new DecorationImage(
-                              image: new ExactAssetImage(_image.path),
-                              fit: BoxFit.cover,
-                            ),
-                            border: Border.all(color: Colors.red, width: 5.0),
-                            borderRadius: new BorderRadius.all(
-                                const Radius.circular(80.0)),
+      key: _formKey,
+      autovalidate: true,
+      child: new ListView(
+        children: <Widget>[
+          new GestureDetector(
+            onTap: () => imagePicker.showDialog(context),
+            child: new Center(
+              child: _image == null
+                  ? new Stack(
+                      children: <Widget>[
+                        new Center(
+                          child: new CircleAvatar(
+                            radius: 80.0,
+                            backgroundImage: fotoPerfil == null
+                                ? AssetImage('assets/img/male.png')
+                                : AssetImage(fotoPerfil),
+                            backgroundColor: const Color(0xFF778899),
                           ),
                         ),
-                ),
-              ),
-              new TextField(
-                keyboardType: TextInputType.text,
-                controller: txtName,
-                decoration: new InputDecoration(
-                    hintText: 'Qual o seu nome?',
-                    labelText: 'Nome',
-                    icon: Icon(Icons.person)),
-                onChanged: (v) => user.name = v,
-              ),
-              new TextField(
-                keyboardType: TextInputType.emailAddress,
-                controller: txtEmail,
-                decoration: new InputDecoration(
-                    hintText: 'Qual o seu e-mail?',
-                    labelText: 'E-mail',
-                    icon: Icon(Icons.email)),
-                onChanged: (v) => user.email = v,
-              ),
-              new TextField(
-                keyboardType: TextInputType.datetime,
-                controller: txtBirthDate,
-                decoration: new InputDecoration(
-                    hintText: 'Qual sua data de nascimento?',
-                    labelText: 'Data de nascimento',
-                    icon: Icon(Icons.date_range)),
-                onChanged: (v) => user.birthDate = v,
-              ),
-              Divider(),
-              new TextField(
-                keyboardType: TextInputType.text,
-                controller: txtUserName,
-                decoration: new InputDecoration(
-                    hintText: 'Escolha um login para acessar o app',
-                    labelText: 'Login',
-                    icon: Icon(Icons.person_pin)),
-                onChanged: (v) => user.username = v,
-              ),
-              new TextField(
-                controller: txtPassword,
-                keyboardType: TextInputType.text,
-                decoration: new InputDecoration(
-                    hintText: 'Escolha uma senha para acessar o app',
-                    labelText: 'Senha',
-                    icon: Icon(Icons.enhanced_encryption)),
-                onChanged: (v) => user.password = v,
-                obscureText: true,
-              ),
-              new Container(
-                width: screenSize.width,
-                child: new RaisedButton(
-                  child: new Text(
-                    'Inserir',
-                    style: new TextStyle(color: Colors.white),
-                  ),
-                  onPressed: this.submit,
-                  color: Colors.blue,
-                ),
-                margin: new EdgeInsets.only(top: 20.0),
-              )
-            ],
+                      ],
+                    )
+                  : new Container(
+                      height: 160.0,
+                      width: 160.0,
+                      decoration: new BoxDecoration(
+                        color: const Color(0xff7c94b6),
+                        image: new DecorationImage(
+                          image: new ExactAssetImage(_image.path),
+                          fit: BoxFit.cover,
+                        ),
+                        border: Border.all(color: Colors.red, width: 5.0),
+                        borderRadius:
+                            new BorderRadius.all(const Radius.circular(80.0)),
+                      ),
+                    ),
+            ),
           ),
-        );
+          new TextField(
+            keyboardType: TextInputType.text,
+            controller: txtName,
+            decoration: new InputDecoration(
+                hintText: 'Qual o seu nome?',
+                labelText: 'Nome',
+                icon: Icon(Icons.person)),
+            onChanged: (v) => user.name = v,
+          ),
+          new TextField(
+            keyboardType: TextInputType.emailAddress,
+            controller: txtEmail,
+            decoration: new InputDecoration(
+                hintText: 'Qual o seu e-mail?',
+                labelText: 'E-mail',
+                icon: Icon(Icons.email)),
+            onChanged: (v) => user.email = v,
+          ),
+          new TextField(
+            keyboardType: TextInputType.datetime,
+            controller: txtBirthDate,
+            decoration: new InputDecoration(
+                hintText: 'Qual sua data de nascimento?',
+                labelText: 'Data de nascimento',
+                icon: Icon(Icons.date_range)),
+            onChanged: (v) => user.birthDate = v,
+          ),
+          Divider(),
+          new TextField(
+            keyboardType: TextInputType.text,
+            controller: txtUserName,
+            decoration: new InputDecoration(
+                hintText: 'Escolha um login para acessar o app',
+                labelText: 'Login',
+                icon: Icon(Icons.person_pin)),
+            onChanged: (v) => user.username = v,
+          ),
+          new TextField(
+            controller: txtPassword,
+            keyboardType: TextInputType.text,
+            decoration: new InputDecoration(
+                hintText: 'Escolha uma senha para acessar o app',
+                labelText: 'Senha',
+                icon: Icon(Icons.enhanced_encryption)),
+            onChanged: (v) => user.password = v,
+            obscureText: true,
+          ),
+          new Container(
+            width: screenSize.width,
+            child: new RaisedButton(
+              child: new Text(
+                'Inserir',
+                style: new TextStyle(color: Colors.white),
+              ),
+              onPressed: (){
+                submit();
+              },
+              color: Colors.blue,
+            ),
+            margin: new EdgeInsets.only(top: 20.0),
+          )
+        ],
+      ),
+    );
   }
 
   @override
@@ -207,21 +206,11 @@ class _UserInsertEdit extends State<UserInsertEdit>
   }
 
   void submit() async {
-    setState(() {
-      _saving = true;
-    });
-
-    new Future.delayed(new Duration(seconds: 4), () {
-      setState(() {
-        _saving = false;
-      });
-    });
-
     if (this.userId == null)
       await UserService().insert('api/person/insert', this.user.toMap());
     else
       await UserService().edit('api/person/edit', this.user.toMap());
 
-    Navigator.pop(context, [true]);
+    Navigator.pop(context, true);
   }
 }
--- lib/pages/user/user-list.dart
+++ lib/pages/user/user-list.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:newflutterproject/domain/user.dart';
 import 'package:newflutterproject/pages/user/user-insert-edit.dart';
 import 'package:newflutterproject/pages/user/user-service.dart';
@@ -9,12 +12,21 @@ class UserList extends StatefulWidget {
 }
 
 class _UserListState extends State<UserList> {
-  getUsers() async {
+  StreamController _userController;
+
+  @override
+  void initState() {
+    _userController = new StreamController();
+    loadUsers();
+    super.initState();
+  }
+
+  Future getUsers() async {
     List<dynamic> users = [];
     var response = await UserService().get(
         '/api/person/list', {'search': null, 'pageSize': 500, 'pageIndex': 1});
 
-    if (this.mounted) {
+    if (mounted) {
       setState(() {
         users = response.map((model) => User.fromJson(model)).toList();
       });
@@ -23,10 +35,17 @@ class _UserListState extends State<UserList> {
     return users;
   }
 
+  loadUsers() {
+    getUsers().then((res) async {
+      _userController.add(res);
+      return res;
+    });
+  }
+
   deleteUser(int userId) async {
     executeDelete() async {
       await UserService().delete('api/person/delete', {'id': userId});
-      await getUsers();
+      await loadUsers();
       Navigator.of(context).pop();
     }
 
@@ -59,49 +78,51 @@ class _UserListState extends State<UserList> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Usuários')),
-      body: Container(
-          child: FutureBuilder(
-              future: getUsers(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (snapshot.data == null) {
-                  return Container(
-                    child: Center(
-                      child: CircularProgressIndicator(),
+      body: StreamBuilder(
+          stream: _userController.stream,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasError) {
+              return Text(snapshot.error);
+            }
+            if (snapshot.data == null) {
+              return Container(
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: snapshot.data[index].image == null
+                          ? AssetImage('assets/img/male.png')
+                          : AssetImage(snapshot.data[index].image),
                     ),
-                  );
-                } else {
-                  return ListView.builder(
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListTile(
-                        leading: CircleAvatar(
-                          backgroundImage: snapshot.data[index].image == null
-                              ? AssetImage('assets/img/male.png')
-                              : AssetImage(snapshot.data[index].image),
-                        ),
-                        title: Text(snapshot.data[index].name),
-                        subtitle: Text(snapshot.data[index].email),
-                        trailing: IconButton(
-                            icon: new Icon(Icons.delete),
-                            color: Colors.red,
-                            onPressed: () async {
-                              await deleteUser(snapshot.data[index].id);
-                            }),
-                        onTap: () {
-                          Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => UserInsertEdit(
-                                          userId: snapshot.data[index].id)))
-                              .then((val) {
-                            if (val != null && val.isNotEmpty) getUsers();
-                          });
-                        },
-                      );
+                    title: Text(snapshot.data[index].name),
+                    subtitle: Text(snapshot.data[index].email),
+                    trailing: IconButton(
+                        icon: new Icon(Icons.delete),
+                        color: Colors.red,
+                        onPressed: () async {
+                          await deleteUser(snapshot.data[index].id);
+                        }),
+                    onTap: () {
+                      Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => UserInsertEdit(
+                                      userId: snapshot.data[index].id)))
+                          .then((val) {
+                        if (val != null) loadUsers();
+                      });
                     },
                   );
-                }
-              })),
+                },
+              );
+            }
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
@@ -109,7 +130,7 @@ class _UserListState extends State<UserList> {
                   MaterialPageRoute(
                       builder: (context) => UserInsertEdit(userId: null)))
               .then((val) {
-            if (val != null && val.isNotEmpty) getUsers();
+            if (val != null) loadUsers();
           });
         },
         child: Icon(Icons.add),
